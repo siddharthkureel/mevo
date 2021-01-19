b5fa1c222b583740e55f0003ea79013ac088c836
Siddharth Kureel <joykureel@gmail.com>
2021-01-20 01:53:32 +1300

    select limit added

--- src/App.js
+++ src/App.js
@@ -6,23 +6,32 @@ import http from './api';
 const App = () => {
     const [term, setTerm] = useState('')
     const [data, setData] = useState(null)
+    const [limit, setLimit] = useState(20)
+    const [disabled, setDisabled] = useState(false) 
     const handleSubmit = async (e) => {
+        setDisabled(true)
         e.preventDefault()
         const trim = term.trim();
         const underscore = trim.replace(/ /g,"_");
-        const response = await (await http.get(`/${underscore}?rel=/r/IsA&limit=100`)).data;
+        const response = await (await http.get(`/${underscore}?rel=/r/IsA&limit=${Number(limit)}`)).data;
         const result = response.edges.filter(result=>result.rel.label==='IsA')
         const sortedResult = result.sort((a, b) => parseFloat(a.weight) - parseFloat(b.weight));
         const objectResult = sortedResult.map(result=>result.end.label)
         const nestedObject = objectResult.reduce((res, key) => ({[key]: res}), {});
         setData(nestedObject)
+        setDisabled(false)
     }
    
     return (
         <div style={styles.container} >
             <form onSubmit={handleSubmit} style={styles.form} >
                 <input onChange={(e)=>setTerm(e.target.value)} value={term} />
-                <button type="submit" >submit</button>
+                <select value={limit} onChange={e=>setLimit(e.target.value)} style={styles.select} > 
+                    <option name="20"> 20</option>
+                    <option name="50">50</option>
+                    <option name="50">100</option>
+                </select>
+                <button disabled={disabled} type="submit" >submit</button>
             </form>
             <div>
                 {
@@ -43,6 +52,9 @@ const styles = {
     },
     form: {
         marginBottom: '20px'
+    },
+    select: {
+        height: '22px'
     }
 }
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -4,5 +4,5 @@ import rateLimit from 'axios-rate-limit';
 export default rateLimit(axios.create({
         baseURL:'https://api.conceptnet.io/c/en'
     }), 
-    { maxRequests: 10, perMilliseconds: 60000, maxRPS: 10 }
+    { maxRequests: 1, perMilliseconds: 60000, maxRPS: 1 }
 )
